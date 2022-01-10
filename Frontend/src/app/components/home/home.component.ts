e24a67f9d71628c8cd7da8035a10832e59d11ccf
mjovetic16 <mjovetic16@raf.rs>
2022-01-10 17:13:49 +0100

    Dodato listanje termina

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from 'src/app/services/home/home.service';
+import { LoginService } from 'src/app/services/login/login.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,95 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  termini = [];
+  filter;
+  username;
+  tableRows;
+
+  constructor(private homeService: HomeService,private loginService: LoginService, private router: Router) { 
+
+    this.filter = {origin:"",destination:"",returnDate:"",departDate:"",all:true,oneWay:true};
+
+  }
 
   ngOnInit(): void {
+    let user = this.homeService.loadUser();
+
+    this.filter = {city:"",hotel:"",returnDate:"",departDate:"",all:true,oneWay:true};
+
+    
+    this.username = user.username;
+
+    console.log(user);
+    
+
+
+    this.loadTermini();
+
   }
+
+  onLogout():void{
+    this.loginService.logout();
+    this.router.navigate(['/']);
+  }
+
+  // loadBookings():void{
+
+  //   let credentials = {username:this.username};
+  //   this.loginService.getUser(credentials).subscribe(data=>{
+  //     let bookings = data['bookings']
+
+  //     this.bookingCount = bookings.length;
+
+  //   },error => {
+  //     console.log(error);
+      
+  //      this.showAlert=true;
+  //      this.alertContent = error.error.message; 
+      
+    
+  //   });
+
+  // }
+
+  loadTermini(){
+    this.homeService.loadPage(this.filter).subscribe(responseData=>{
+      
+      let list = responseData['content'];
+
+      this.termini = list;
+
+    
+
+      this.tableRows = [];
+      for (let index = 0; index < list.length; index++) {
+        const element = list[index];
+
+        this.tableRows[element.id] = element;
+        this.tableRows[element.id].value = 1;
+        
+      }
+
+      console.log(this.tableRows);
+      
+      
+      
+      
+      
+    },error => {
+      console.log(error);
+    });
+
+  }
+
+  onChangeCity(){
+    this.loadTermini();
+  }
+
+  onChangeHotel(){
+    this.loadTermini();
+  }
+
+
 
 }
--- Frontend/src/app/services/home/home.service.ts
+++ Frontend/src/app/services/home/home.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  private readonly userUrl = "http://localhost:8083/users/user";
+  private readonly reservationUrl = "http://localhost:8083/main/reservation";
+
+
+  constructor(private http: HttpClient) { }
+
+  loadUser(){
+
+    return JSON.parse(localStorage.getItem("user"))
+
+  }
+
+ 
+
+  loadPage(filter){
+
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', "Bearer "+localStorage.getItem('jwt'));
+
+  
+
+    return this.http.get(this.reservationUrl+"/termin",{headers:headers});
+
+  }
 }
